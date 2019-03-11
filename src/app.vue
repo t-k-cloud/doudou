33241b0cc69881ae7010fe0214073bbd7de463a6
t-k- <clock126@126.com>
2019-03-11 13:20:57 +0800

    frontend setup.

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+<h2>hello</h2>
+</div>
+</template>
+
+<script>
+import axios from 'axios' /* AJAX request lib */
+
+export default {
+  methods: {
+  },
+  mounted: function () {
+  },
+  computed: {
+  },
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
